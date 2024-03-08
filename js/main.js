c03d04042bcb65323eb51acebafa87306aeeb835
Goye1 <manuel.goyenechea@gmail.com>
2024-03-08 02:03:44 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,133 @@
+(function ($) {
+    "use strict";
+
+    var clicHecho = false;
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
+
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+
+
+    var clicAutomaticoEnlace = function () {
+
+        if(!clicHecho){
+        // Obtén el elemento <a> por su id
+        var enlace = document.getElementById("enlace");
+
+        // Obtén las dimensiones y la posición del elemento en relación con la ventana gráfica
+        var rect = enlace.getBoundingClientRect();
+
+        // Verifica si el elemento está completamente visible en la ventana gráfica
+        if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+            // Haz clic en el enlace
+            enlace.click();
+            clicHecho = true;
+        }
+    };
+
+}
+    
+    // Llama a la función para hacer clic automático en el enlace cuando sea necesario
+    $(window).scroll(function () {
+        clicAutomaticoEnlace();
+    });
+
+
+
+
+
+    
+})(jQuery);
+
